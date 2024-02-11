a67d98339babf3b10ddf0a22063c174224578d8a
efekos <hefeolgun10@gmail.com>
2024-02-11 20:14:48 +0300

    change trackingcompass class

--- src/main/java/me/efekos/awakensmponline/items/TrackingCompass.java
+++ src/main/java/me/efekos/awakensmponline/items/TrackingCompass.java
@@ -2,47 +2,93 @@ package me.efekos.awakensmponline.items;
 
 import me.efekos.awakensmponline.Main;
 import me.efekos.awakensmponline.utils.DateUtils;
-import me.efekos.simpler.items.CustomItem;
+import me.efekos.simpler.items.*;
 import me.efekos.simpler.translation.TranslateManager;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 public class TrackingCompass extends CustomItem {
-    private Player whoToTrack;
-    private Player whoBelongs;
-    private Date expiresAt;
+    @SaveField("track")
+    private String whoToTrack;
+    @SaveField("belong")
+    private String whoBelongs;
+    @SaveField(value = "expire",fieldType = FieldType.LONG)
+    private long expiresAt;
+
+    public Player getWhoToTrack() {
+        return Bukkit.getPlayer(UUID.fromString(whoToTrack));
+    }
+
+    public void setWhoToTrack(Player whoToTrack) {
+        this.whoToTrack = whoToTrack.getUniqueId().toString();
+    }
+
+    public Player getWhoBelongs() {
+        return Bukkit.getPlayer(UUID.fromString(whoBelongs));
+    }
+
+    public void setWhoBelongs(Player whoBelongs) {
+        this.whoBelongs = whoBelongs.getUniqueId().toString();
+    }
+
+    public Date getExpiresAt() {
+        return new Date(expiresAt);
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt.getTime();
+    }
+
+    private final Consumer<ItemStack> appearance = stack -> {
+        stack.setType(Material.COMPASS);
+        CompassMeta meta = (CompassMeta) new ItemStack(Material.COMPASS).getItemMeta();
+        assert meta != null;
+        meta.setLodestone(getWhoToTrack().getLocation());
+        meta.setDisplayName(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.name", "&eTracking Compass")));
+        meta.setLore(Arrays.asList(
+                TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.description.attach", "&6Attached to &d%player%").replace("%player%", getWhoToTrack().getName())),
+                TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.description.own", "&6Owned by &d%tracker%").replace("%tracker%", getWhoBelongs().getName())),
+                TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.description.expire", "&6Expires at &b%date%").replace("%date%", DateUtils.translateDate(getExpiresAt())))
+        ));
+
+        stack.setItemMeta(meta);
+    };
 
     public TrackingCompass() {
+        super(new NamespacedKey(Main.getInstance(),"tracking_compass"),stack -> {});
     }
 
     public TrackingCompass(Player whoToTrack, Player whoBelongs, Date expiresAt) {
-        this.whoToTrack = whoToTrack;
-        this.whoBelongs = whoBelongs;
-        this.expiresAt = expiresAt;
+        this();
+        setWhoToTrack(whoToTrack);
+        setWhoBelongs(whoBelongs);
+        setExpiresAt(expiresAt);
+        //TODO wait for library to run #setAppearance
     }
 
-    @Listen
-    @RightClick
+    @HandleEvent(HandleType.PICKUP)
     public void onRightClick(PlayerInteractEvent event) {
         Player p = event.getPlayer();
-        if (new Date().after(expiresAt)) {
-            p.sendMessage(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.expired", "&cThis compass is expired at &a%date%&c.").replace("%date%", DateUtils.translateDate(expiresAt))));
+        if (new Date().after(getExpiresAt())) {
+            p.sendMessage(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.expired", "&cThis compass is expired at &a%date%&c.").replace("%date%", DateUtils.translateDate(getExpiresAt()))));
             return;
         }
-        if (!p.equals(whoBelongs)) {
+        if (!p.equals(getWhoBelongs())) {
             p.sendMessage(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.not-owner", "&cYou are not the owner of this compass.")));
             return;
         }
-        if (!whoToTrack.isOnline()) {
-            p.sendMessage(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.not-online", "&a%player% &cis not online right now.").replace("%player%", whoToTrack.getName())));
+        if (!getWhoToTrack().isOnline()) {
+            p.sendMessage(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.not-online", "&a%player% &cis not online right now.").replace("%player%", getWhoToTrack().getName())));
         }
 
 
@@ -51,37 +97,11 @@ public class TrackingCompass extends CustomItem {
         CompassMeta meta = (CompassMeta) stack.getItemMeta();
         assert meta != null;
         meta.setLodestoneTracked(false);
-        meta.setLodestone(whoToTrack.getLocation());
+        meta.setLodestone(getWhoToTrack().getLocation());
         stack.setItemMeta(meta);
         p.getInventory().setItemInMainHand(stack);
 
         p.sendMessage(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.refreshed", "&aRefreshed location!")));
 
-    }
-
-
-    @Override
-    public @NotNull String getId() {
-        return "tracking_compass";
-    }
-
-    @Override
-    public @NotNull ItemMeta getDefaultMeta() {
-        CompassMeta meta = (CompassMeta) new ItemStack(Material.COMPASS).getItemMeta();
-        assert meta != null;
-        meta.setLodestone(whoToTrack.getLocation());
-        meta.setDisplayName(TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.name", "&eTracking Compass")));
-        meta.setLore(Arrays.asList(
-                TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.description.attach", "&6Attached to &d%player%").replace("%player%", whoToTrack.getName())),
-                TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.description.own", "&6Owned by &d%tracker%").replace("%tracker%", whoBelongs.getName())),
-                TranslateManager.translateColors(Main.LANG.getString("items.tracking_compass.description.expire", "&6Expires at &b%date%").replace("%date%", DateUtils.translateDate(expiresAt)))
-        ));
-
-        return meta;
-    }
-
-    @Override
-    public @NotNull Material getMaterial() {
-        return Material.COMPASS;
     }
 }
